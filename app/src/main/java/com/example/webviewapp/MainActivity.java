c96c6bc46c615408cc585a24018f2b1f97978f1d
a97marbr <a97marbr@gmail.com>
2020-03-16 12:33:44 +0100

    Updated the seed code for spring 2020

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,11 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void showExternalWebPage(){
+        // TODO: Add your code for showing external web page here
+    }
+
+    public void showInternalWebPage(){
+        // TODO: Add your code for showing internal web page here
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /*
+        * Rename your App. Tip: Values->Strings
+        * Enable Internet access for your App. Tip: Manifest
+        * Create a WebView element in the layout file content_main.xml
+        * Give the WebView element ID "my_webview"
+        -- Commit and push to your github fork
+        * Create a private member variable called "myWebView" of type WebView
+        * Locate the WebView element created in step 1 using the ID created in step 2
+        * Create a new WebViewClient to attach to our WebView. This allows us to
+          browse the web inside our app.
+        -- Commit and push to your github fork
+        * Enable Javascript execution in your WebViewClient
+        * Enter the url to load in our WebView
+        -- Commit and push to your github fork
+        * Move the code that loads a URL into your WebView into the two methods
+          "showExternalWebPage()" and "showInternalWebPage()".
+        * Call the "showExternalWebPage()" / "showInternalWebPage()" methods
+          when you select menu options "External Web Page" or "Internal Web Page"
+          respectively
+        -- Commit and push to your github fork
+        * Take two screenshots using the "Take a screenshot" tool in the AVD
+           showing your App. One (1) screenshot showing your internal web page and
+           one (1) screenshot showing your external web page.
+        */
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +77,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_external_web) {
+            Log.d("==>","Will display external web page");
+            return true;
+        }
+
+        if (id == R.id.action_internal_web) {
+            Log.d("==>","Will display internal web page");
             return true;
         }
 
